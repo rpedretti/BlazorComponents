0ae984954e1d8aa395d26fe9376443fa14e13e0e
Rafael Pedretti <rpedretti@ciandt.com>
2018-08-12 17:29:18 -0300

    Migrating JS to new JSInterop

---
+++ RPedretti.Blazor.Components/Carousel/Carousel.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPedretti.Blazor.Components.Carousel
+{
+    public class CarouselBase : BlazorComponent
+    {
+        protected bool HasNext { get; set; }
+        protected bool HasPrev { get; set; }
+
+        public void GoToNext()
+        {
+
+        }
+
+        public void GoToPrev()
+        {
+
+        }
+    }
+}
---
+++ RPedretti.Blazor.Components/content/js/ambient-light.js
@@ -0,0 +1,26 @@
+﻿listeners = [];
+
+function registerListener(listener) {
+    listeners.push(listener);
+}
+
+function removeListener(listener) {
+    listeners.remove(listener);
+}
+
+if ('AmbientLightSensor' in window) {
+    const sensor = new AmbientLightSensor();
+    sensor.onreading = () => {
+        listeners.foreach(l => l.InvokeMethodAsync('CheckLight', sensor.illuminance));
+    };
+    sensor.onerror = (event) => {
+        console.log(event.error.name, event.error.message);
+    };
+    sensor.start();
+} else {
+    console.debug('no api');
+}
+
+window.addEventListener('devicelight', function (event) {
+    console.log(event.value);
+});
